f3805beae6dd1079295292efa0375aae83486147
Erik Nikli <erik200203@gmail.com>
2024-02-28 17:38:45 +0100

    Create projects

---
+++ Q7VQV4/Calculator/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ Q7VQV4/CalculatorTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace CalculatorTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
