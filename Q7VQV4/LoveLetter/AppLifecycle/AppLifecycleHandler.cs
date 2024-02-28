c32e4d9fc882af77c02fea628d735103ec503f78
Erik Nikli <erik200203@gmail.com>
2024-02-28 18:29:35 +0100

    Add Lifecycle handler

---
+++ Q7VQV4/LoveLetter/AppLifecycle/AppLifecycleHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace LoveLetter.Bootstrap;
+
+public class AppLifecycleHandler : IHostedService
+{
+    private readonly IHostApplicationLifetime _appLifeTime;
+
+    public AppLifecycleHandler(IHostApplicationLifetime appLifeTime)
+    {
+        this._appLifeTime = appLifeTime;
+        this._appLifeTime.ApplicationStarted.Register(OnStart);
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+
+    private void OnStart()
+    {
+        Console.WriteLine("App has started.");
+    }
+}
