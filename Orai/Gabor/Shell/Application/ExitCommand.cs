a8e7a6ceb4496299cb89fb5bc702b98decab10e8
Gabor Ruzsinszki <gabor.ruzsinszki@evosoft.com>
2024-03-08 10:16:53 +0100

    Előző órai anyagok

---
+++ Orai/Gabor/Shell/Application/ExitCommand.cs
@@ -0,0 +1,19 @@
+﻿using Shell.Infrastructure;
+
+namespace Shell.Application
+{
+    internal class ExitCommand : IShellCommand
+    {
+        public string Name => "exit";
+
+        //public string Name 
+        //{
+        //    get { return "exit"; }
+        //}
+
+        public void Execute(IHost host, string[] args)
+        {
+            host.Exit();
+        }
+    }
+}
---
+++ Orai/Gabor/Shell/UserInterface/CommandProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Shell.Application;
+using Shell.Infrastructure;
+
+namespace Shell.UserInterface
+{
+    internal class CommandProvider
+    {
+        public IShellCommand[] Commands
+        {
+            get;  
+        }
+
+        public CommandProvider()
+        {
+            Commands = new IShellCommand[]
+                {
+                    new ExitCommand(),
+                };
+        }
+    }
+}
