7c946eeca5dd6e5fb9fdc66051a6ef69eb5297bc
Ruzsinszki <gabor.ruzsinszki@evosoft.com>
2024-03-22 13:39:43 +0100

    2024.03.21 + 2024.03.14 anyag

---
+++ Orai/Szandi/Matek/Matek/PaymentProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matek
+{
+    public class PaymentProcessor
+    {
+        private readonly IPaymentGateway _paymentGateway;
+
+        public PaymentProcessor(IPaymentGateway paymentGateway)
+        {
+            _paymentGateway = paymentGateway;
+        }
+
+        public bool ProcessPayment(double amount)
+        {
+            Console.WriteLine($"Processing payment of {amount}...");
+            //_paymentGateway.ProcessPayment(amount);
+            return _paymentGateway.ProcessPayment(amount);
+        }
+    }
+}
