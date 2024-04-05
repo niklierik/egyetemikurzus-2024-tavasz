2a9d96361ea5214ff61927d655a7be1755c931d7
Ruzsinszki <gabor.ruzsinszki@evosoft.com>
2024-04-05 08:53:09 +0200

    XML és JSON szerializáció

---
+++ Orai/Gabor/Shell/Application/XmlCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+using Shell.Infrastructure;
+
+namespace Shell.Application;
+
+internal class XmlCommand : IShellCommand
+{
+    public string Name => "xml";
+
+    public void Execute(IHost host, string[] args)
+    {
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                                   "test.xml");
+
+        using (var stream =  File.Create(path))
+        {
+            try
+            {
+                var serializer = new DagattMacskaSerializer();
+                serializer.SerializeToXml(stream, new Domain.DagattMacska
+                {
+                    Name = "cirmi"
+                });
+            }
+            catch(IOException ex)
+            {
+                host.WriteLine(ex.Message);
+            }
+        }
+    }
+}
+
+internal class JsonCommand : IShellCommand
+{
+    public string Name => "json";
+
+    public void Execute(IHost host, string[] args)
+    {
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                                   "test.json");
+
+        using (var stream = File.Create(path))
+        {
+            try
+            {
+                var serializer = new DagattMacskaSerializer();
+                serializer.SerializeToJson(stream, new Domain.DagattMacska
+                {
+                    Name = "cirmi"
+                });
+            }
+            catch (IOException ex)
+            {
+                host.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Orai/Gabor/Shell/Infrastructure/DagattMacskaSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Text.Json;
+
+using Shell.Domain;
+
+namespace Shell.Infrastructure
+{
+    internal class DagattMacskaSerializer
+    {
+        public void SerializeToXml(Stream target, DagattMacska instance)
+        {
+            var serializer = new XmlSerializer(typeof(DagattMacska));
+            serializer.Serialize(target, instance);
+        }
+
+        public void SerializeToJson(Stream target, DagattMacska instance)
+        {
+            JsonSerializer.Serialize(target, instance, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            });
+        }
+    }
+}
