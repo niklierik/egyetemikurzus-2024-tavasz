e784af4d63eef387219fa09f245630efef9b46a5
Ruzsinszki <gabor.ruzsinszki@evosoft.com>
2024-04-16 14:44:07 +0200

    Linq

--- Orai/Gabor/Shell/Application/JsonCommand.cs
+++ Orai/Gabor/Shell/Application/JsonCommand.cs
@@ -2,26 +2,26 @@
 
 namespace Shell.Application;
 
-internal class XmlCommand : IShellCommand
+internal class JsonCommand : IShellCommand
 {
-    public string Name => "xml";
+    public string Name => "json";
 
     public void Execute(IHost host, string[] args)
     {
         string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
-                                   "test.xml");
+                                   "test.json");
 
-        using (var stream =  File.Create(path))
+        using (var stream = File.Create(path))
         {
             try
             {
                 var serializer = new DagattMacskaSerializer();
-                serializer.SerializeToXml(stream, new Domain.DagattMacska
+                serializer.SerializeToJson(stream, new Domain.DagattMacska
                 {
                     Name = "cirmi"
                 });
             }
-            catch(IOException ex)
+            catch (IOException ex)
             {
                 host.WriteLine(ex.Message);
             }
---
+++ Orai/Gabor/Shell/Application/LinqDemoCommand.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Shell.Infrastructure;
+
+namespace Shell.Application
+{
+    internal class LinqDemoCommand : IShellCommand
+    {
+        public string Name => "linq";
+
+        public void Execute(IHost host, string[] args)
+        {
+            int[] elements = new int[100];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                elements[i] = Random.Shared.Next(0, 100);
+            }
+
+            //Query sintax
+            var strings = from element in elements
+                          orderby element descending
+                          select ToFizzbuzz(element);
+
+            //Lambda syntax
+            var strings2 = elements
+                .Select(element =>
+            {
+                if (element % 5 == 0 && element % 3 == 0)
+                    return "FizzBuzz";
+                else if (element % 3 == 0)
+                    return "Fizz";
+                else if (element % 5 == 0)
+                    return "Buzz";
+                else
+                    return element.ToString();
+            }).OrderBy(s => s[0]);
+
+            foreach (var s in strings.ToArray())
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        private string ToFizzbuzz(int element)
+        {
+            if (element % 5 == 0 && element % 3 == 0)
+                return "FizzBuzz";
+            else if (element % 3 == 0)
+                return "Fizz";
+            else if (element % 5 == 0)
+                return "Buzz";
+            else
+                return element.ToString();
+        }
+    }
+}
